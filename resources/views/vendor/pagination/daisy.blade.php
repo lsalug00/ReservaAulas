d19315a7ce24b8af1187d7f4c5b269db2753a5b4
lsalug00 <lidiasaludes@gmail.com>
2025-05-19 14:18:48 +0200

    responsive paginacion

--- resources/views/vendor/pagination/daisy.blade.php
+++ resources/views/vendor/pagination/daisy.blade.php
@@ -1,34 +1,66 @@
 @if ($paginator->hasPages())
-    <div class="join mt-6 justify-center flex">
-        {{-- Página anterior --}}
-        @if ($paginator->onFirstPage())
-            <span class="join-item btn btn-disabled">«</span>
-        @else
-            <a href="{{ $paginator->previousPageUrl() }}" class="join-item btn">«</a>
-        @endif
-
-        {{-- Números de página --}}
-        @foreach ($elements as $element)
-            @if (is_string($element))
-                <span class="join-item btn btn-disabled">{{ $element }}</span>
+    <div class="mt-6 flex justify-center">
+        <div class="join items-center">
+            {{-- Primera página --}}
+            @if (!$paginator->onFirstPage())
+                <a href="{{ $paginator->url(1) }}" class="join-item btn">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 24 24" fill="none" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 19L6 12l5-7M18 19l-5-7 5-7" />
+                    </svg>
+                </a>
             @endif
 
-            @if (is_array($element))
-                @foreach ($element as $page => $url)
-                    @if ($page == $paginator->currentPage())
-                        <span class="join-item btn btn-active btn-primary">{{ $page }}</span>
-                    @else
-                        <a href="{{ $url }}" class="join-item btn">{{ $page }}</a>
-                    @endif
-                @endforeach
+            {{-- Anterior --}}
+            @if ($paginator->onFirstPage())
+                <span class="join-item btn btn-disabled">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none"
+                         viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M15 19l-7-7 7-7"/>
+                    </svg>
+                </span>
+            @else
+                <a href="{{ $paginator->previousPageUrl() }}" class="join-item btn">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none"
+                         viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M15 19l-7-7 7-7"/>
+                    </svg>
+                </a>
             @endif
-        @endforeach
 
-        {{-- Página siguiente --}}
-        @if ($paginator->hasMorePages())
-            <a href="{{ $paginator->nextPageUrl() }}" class="join-item btn">»</a>
-        @else
-            <span class="join-item btn btn-disabled">»</span>
-        @endif
+            {{-- Página actual (activa pero sin acción) --}}
+            <button class="join-item btn btn-active cursor-default pointer-events-none">
+                {{ $paginator->currentPage() }} / {{ $paginator->lastPage() }}
+            </button>
+
+            {{-- Siguiente --}}
+            @if ($paginator->hasMorePages())
+                <a href="{{ $paginator->nextPageUrl() }}" class="join-item btn">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none"
+                         viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M9 5l7 7-7 7"/>
+                    </svg>
+                </a>
+            @else
+                <span class="join-item btn btn-disabled">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none"
+                         viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M9 5l7 7-7 7"/>
+                    </svg>
+                </span>
+            @endif
+
+            {{-- Última página --}}
+            @if ($paginator->currentPage() < $paginator->lastPage())
+                <a href="{{ $paginator->url($paginator->lastPage()) }}" class="join-item btn">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 24 24" fill="none" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5l5 7-5 7M6 5l5 7-5 7" />
+                    </svg>
+                </a>
+            @endif
+        </div>
     </div>
 @endif
